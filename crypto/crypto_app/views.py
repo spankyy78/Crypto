ba86c32de1d6c99407bca8a409df05a924f25792
Tim Detourbet <tim.detournet@epita.fr>
2018-08-28 19:11:17 +0200

    [CRYPTO_APP] - sign up added

--- crypto/crypto_app/views.py
+++ crypto/crypto_app/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
-from .models import Users
+from django.urls import reverse_lazy
+from django.views import generic
 
 def home(request):
     return render(request, 'home.html')
@@ -12,6 +14,11 @@ def login(request):
 
 def base(request):
     return render(request, 'base.html')
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
 
 def signup(request):
     return render(request, 'signup.html')
